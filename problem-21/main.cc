547cbb066bc74fcd62fd4eef850cd69c2cabf78e
Jens Hoffmann <xmcpam@gmail.com>
2015-05-26 08:22:55 +0200

    Problem 21

---
+++ problem-21/main.cc
@@ -0,0 +1,43 @@
+// Copyright 2015, Jens Hoffmann
+//
+// Amicable numbers
+// Problem 21
+//
+// Let d(n) be defined as the sum of proper divisors of n (numbers less than n
+// which divide evenly into n).  If d(a) = b and d(b) = a, where a ≠ b, then a
+// and b are an amicable pair and each of a and b are called amicable numbers.
+//
+// For example, the proper divisors of 220 are 1, 2, 4, 5, 10, 11, 20, 22, 44,
+// 55 and 110; therefore d(220) = 284. The proper divisors of 284 are 1, 2, 4,
+// 71 and 142; so d(284) = 220.
+//
+// Evaluate the sum of all the amicable numbers under 10000.
+//
+#include <iostream>
+
+
+int d(int x) {
+    int ret = 0;
+    for (int i = 1; i <= x/2; ++i) {
+        if (x % i == 0) ret += i;
+    }
+    return ret;
+}
+
+
+int main() {
+    int result = 0;
+    
+    for (int i = 1; i < 10000; ++i) {
+        int d_i = d(i);
+        if (i == d_i) continue;
+        if (d(d_i) == i) {
+            result += i;
+            std::cout << i << " is an amicable number" << std::endl;
+        }
+    }
+    std::cout << "Result: " << result << std::endl;
+
+    return 0;
+}
+
